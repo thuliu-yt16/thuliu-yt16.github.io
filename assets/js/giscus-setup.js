9cb28225d525e81ebdbb04cf530899b7425a8fc9
thuliu-yt16 <lytlogic@gmail.com>
2026-01-27 06:03:21 +0000

    Deploying to gh-pages from @ thuliu-yt16/thuliu-yt16.github.io@51fe04923b576d7764ccfc25885e823cbea03bc7 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "thuliu-yt16/thuliu-yt16.githhub.io",
+    "data-repo": "thuliu-yt16/thuliu-yt16.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
